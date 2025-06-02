d92e64dec02cde5779c3eb840ca1f5111a8d8a26
Shahin Parveen <shahinp2512@gmail.com>
2025-06-02 11:57:02 +0530

    check

--- utils/facebook.js
+++ utils/facebook.js
@@ -110,7 +110,8 @@ class FacebookSDK {
             reject(new Error('Facebook login was cancelled or failed'));
           }
         }, {
-          scope: 'pages_show_list,pages_read_engagement,read_insights,business_management'
+          //scope: 'pages_show_list,pages_read_engagement,read_insights,business_management'
+          scope: 'public_profile,email'
         });
       });
       
